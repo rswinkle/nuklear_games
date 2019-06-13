59669c1e6a078060081c18159fe0d2998ae9b2da
Robert Winkler <rob121618@gmail.com>
2019-06-12 21:50:14 -0700

    Working MVP of sorts

--- minesweeper.c
+++ minesweeper.c
@@ -20,11 +20,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 400
 
 
 int handle_events(struct nk_context* ctx);
+void reveal(int* board, int* board2, int i);
 
 int main()
 {
@@ -33,7 +34,7 @@ int main()
 	struct nk_context* ctx;
 
 	if (SDL_Init(SDL_INIT_VIDEO) == -1) {
-		printf( "Can't init SDL:  %s\n", SDL_GetError( ) );
+		printf("Can't init SDL:  %s\n", SDL_GetError( ));
 		return 1;
 	}
 
@@ -41,6 +42,9 @@ int main()
                            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                            WINDOW_WIDTH, WINDOW_HEIGHT,
                            SDL_WINDOW_SHOWN|SDL_WINDOW_ALLOW_HIGHDPI);
+
+    int scr_w = WINDOW_WIDTH;
+    int scr_h = WINDOW_HEIGHT;
 
 	if (!win) {
 		printf("Can't create window: %s\n", SDL_GetError());
@@ -58,8 +62,48 @@ int main()
 		return 1;
 	}
 
+	int gui_flags = 0;
 
 	struct nk_color background = { 0, 0, 0, 255 };
+
+
+	enum { BOMB = -1, EMPTY };
+	enum { HIDDEN, SHOWN };
+	int board[100] = { EMPTY };
+	int board2[100] = { HIDDEN };
+	int tmp;
+	for (int i=0; i<10; ++i) {
+		tmp = rand() % 100;
+		while (board[tmp] == BOMB)
+			tmp = rand() % 100;
+		board[tmp] = BOMB;
+	}
+
+	for (int i=0; i<10; ++i) {
+		for (int j=0; j<10; ++j) {
+			if (board[i*10+j] == BOMB) {
+				int k = (i-1 < 0) ? 0 : i-1;
+				int g = (j-1 < 0) ? 0 : j-1;
+				int ek = (i+2 > 10) ? 10 : i+2;
+				int eg = (j+2 > 10) ? 10 : j+2;
+
+				int sg = g;
+				//printf("%d %d, going %d %d to %d %d\n", i, j, k, g, ek, eg);
+				for (; k<ek; ++k) {
+					for (g=sg; g<eg; ++g) {
+						//printf("%d %d\n", k, g);
+						if (board[k*10+g] != BOMB)
+							board[k*10+g]++;
+					}
+				}
+			}
+		}
+	}
+
+	char* nums[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+	struct nk_color hidden = {128,128,128, 255};
+	struct nk_color red = {128,0,0, 255};
+
 
 	while (1) {
 		if (handle_events(ctx)) {
@@ -67,19 +111,22 @@ int main()
 		}
 
 		if (nk_begin(ctx, "Minesweeper", nk_rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
-			NK_WINDOW_BORDER|NK_WINDOW_TITLE))
+			gui_flags))
 		{
 			int i;
-			static int selected[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1};
 
-			nk_layout_row_static(ctx, 50, 50, 4);
-			for (i = 0; i < 16; ++i) {
-				if (nk_selectable_label(ctx, "Z", NK_TEXT_CENTERED, &selected[i])) {
-					int x = (i % 4), y = i / 4;
-					if (x > 0) selected[i - 1] ^= 1;
-					if (x < 3) selected[i + 1] ^= 1;
-					if (y > 0) selected[i - 4] ^= 1;
-					if (y < 3) selected[i + 4] ^= 1;
+			nk_layout_row_static(ctx, scr_w/12, scr_h/12, 10);
+			for (i = 0; i < 100; ++i) {
+
+				if (board2[i] == HIDDEN) {
+					if (nk_button_color(ctx, hidden)) {
+						board2[i] = SHOWN;
+					}
+				} else {
+					if (board[i] != BOMB)
+						nk_button_label(ctx, nums[board[i]]);
+					else
+						nk_button_color(ctx, red);
 				}
 			}
 		}
@@ -98,13 +145,24 @@ int main()
 
 }
 
+
+void reveal(int* board, int* board2, int i)
+{
+}
+
 int handle_events(struct nk_context* ctx)
 {
 	SDL_Event e;
 	nk_input_begin(ctx);
 	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT)
+		switch (e.type) {
+		case SDL_QUIT:
 			return 1;
+		case SDL_KEYUP:
+			if (e.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+				return 1;
+		}
+
 		nk_sdl_handle_event(&e);
 	}
 	nk_input_end(ctx);
